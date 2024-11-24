5fdb1b07f4984660b98af788f869c50f25983b7e
MrShotDead <mrshotdead@gmail.com>
2024-11-24 16:59:48 +0000

    Update Question_Game.py

--- Question_Game.py
+++ Question_Game.py
@@ -1,4 +1,6 @@
 import pgzrun
+game_over = False
+#import crash_trigger
 WIDTH=1000
 HEIGHT=600
 question = []
@@ -10,6 +12,11 @@ ans_4 = Rect (600,400,325,175)
 timer = Rect (405,175,175,175)
 ans = [ans_1,ans_2,ans_3,ans_4]
 not_a_skip_box = Rect (405,400,175,175)
+def quedtions():
+    file = open("txt_a.txt","r")
+    global question
+    for i in file:
+        question.append(i)
 def draw():
     screen.clear()
     screen.fill("black")
@@ -25,29 +32,41 @@ def draw():
     screen.draw.textbox(me_no_english[2],ans_2,color = "blue")
     screen.draw.textbox(me_no_english[3],ans_3,color = "blue")
     screen.draw.textbox(me_no_english[4],ans_4,color = "blue")
+    screen.draw.textbox("skip",not_a_skip_box, color = "green")
+    if game_over == True:
+        screen.blit("middle_finger-removebg-preview (1).png",(400,300))
+def write():
+    global question
+    return question.pop(0).split(",")
 def reddit():
     global me_no_english
     if question:
         me_no_english = write()
-        print (question)    
+         print (question)
 def on_mouse_down(pos):
     number = 1
     for i in ans:
         if i.collidepoint(pos):
             if number==int(me_no_english[5]):
-                 pass
+                reddit()
             else:
-                    print ("HA YOU THINK YOUR SOOOOOOOOO SMART DONT YA KYS ILHAS TEEZY YA ZINGY")
+                m_f()
+#                    print ("HA YOU THINK YOUR SOOOOOOOOO SMART DONT YA KYS ILHAS TEEZY YA ZINGY")
         number = number+1
-def quedtions():
-    file = open("txt_a.txt","r")
-    global question
-    for i in file:
-        question.append(i)
+    if not_a_skip_box.collidepoint(pos):
+        skip()
+def skip():
+    global me_no_english
+    if question:
+        me_no_english = write()
+        print (question)
 quedtions()
-def write():
-    global question
-    return question.pop(0).split(",")
+def m_f():
+    global me_no_english
+    global game_over
+    me_no_english = ["place image here","-","-","-","-","-"]
+    game_over = True
 me_no_english = write()
 print (question)
+print (me_no_english)
 pgzrun.go()
